37d9cfa64592900537054a7f67c6597d3397d65f
shapiro7 <57553889+shapiro7@users.noreply.github.com>
2020-05-16 17:45:52 +0900

    asdf
    asdf

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,13 +14,6 @@ public class MainActivity extends AppCompatActivity {
 
     // 안녕하세요
     // sasdfasdfasdf
-=======
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-               super.onCreate(savedInstanceState);
-               setContentView(R.layout.activity_main);
-               //sadfasdf
 
     }//asdfsafd
->>>>>>> 3f048d183f8a15abe5f5821767ce8c150016ae1c
-}
+
